8887c06729010157221bbc986bb12fec7630b8f6
HaoYi <491493118@qq.com>
2019-02-22 18:45:35 +0800

    滤镜功能

--- tabbar_demo/lib/main.dart
+++ tabbar_demo/lib/main.dart
@@ -21,8 +21,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      // home:BottomNavigationWidget()
-      home:FirstPage()
+      home:BottomNavigationWidget()
+      // home:FirstPage()
     );
   }
 }
--- tabbar_demo/lib/pages/email_screen.dart
+++ tabbar_demo/lib/pages/email_screen.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-
+import 'package:tabbar_demo/frosted_glass_demo.dart';
 class EmailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar:AppBar(
-        title: Text('EmailScreen'),
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
       ),
-      body:Center(
-        child: Text('EmailScreen'),
-      )
+      home: Scaffold(
+        body: FrostedGlassDemo(),
+      ),
     );
   }
 }
