906324a9971d0bbb831c3c62143acdc70ca04adc
HaoYi <491493118@qq.com>
2019-02-23 19:50:42 +0800

    添加搜索框

---
+++ searchbardemo/lib/search_bar_demo.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'asset.dart';
+class SearchBarDemo extends StatefulWidget {
+  _SearchBarDemoState createState() => _SearchBarDemoState();
+}
+
+class _SearchBarDemoState extends State<SearchBarDemo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Text('Serachbar'),
+         actions: <Widget>[
+           IconButton(
+             icon: Icon(Icons.search),
+             onPressed: (){
+               print('开始搜索');
+             },
+           )
+         ],
+       ),
+    );
+  }
+}
