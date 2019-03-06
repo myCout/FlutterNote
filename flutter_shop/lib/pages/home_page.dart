1385139e323437c9cd15a97d6655cc82ce41ac09
HaoYi <yuandu86@163.com>
2019-03-06 22:03:21 +0800

    轮播组件简单使用；屏幕适配组件使用；

--- flutter_shop/lib/pages/home_page.dart
+++ flutter_shop/lib/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_shop/service/service_method.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
+import 'dart:convert';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
@@ -19,11 +23,26 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    getHttp();
+    // getHttp();
     return Scaffold(
       appBar: AppBar(title: Text('百姓生活')),
-      body:SingleChildScrollView(
-        child: Text(homePageContent),
+      body:FutureBuilder(//用来等待异步请求
+          future: getHomePageContent(),
+          builder: (context,snapshot){
+            if (snapshot.hasData) {
+              var data =json.decode(snapshot.data.toString());
+              List<Map> swiperDataList = (data['data']['slides'] as List).cast();//轮播
+              return Column(
+                children: <Widget>[
+                  SwiperDiy(swiperDataList:swiperDataList)
+                ],
+              );
+            }else{
+              return Center(
+                child: Text('加载中'),
+              );
+            }
+          },
       )
     );
   }
@@ -39,5 +58,30 @@ void getHttp() async {
     return print(response);
   } catch (e) {
     return print(e);
+  }
+}
+
+// 首页轮播组件编写
+class SwiperDiy extends StatelessWidget {
+  final List swiperDataList;
+  SwiperDiy({Key key,this.swiperDataList}):super(key:key);
+  @override
+  Widget build(BuildContext context) {
+    ScreenUtil.instance =ScreenUtil(width: 750,height: 1334)..init(context);
+    print('设备宽度:${ScreenUtil.screenWidth}');
+     print('设备高度:${ScreenUtil.screenHeight}');
+      print('设备像素密度:${ScreenUtil.pixelRatio}');
+    return Container(
+      height: ScreenUtil().setHeight(333),
+      width: ScreenUtil().setWidth(750),
+      child: Swiper(
+        itemBuilder: (BuildContext context,int index){
+          return Image.network("${swiperDataList[index]['image']}",fit:BoxFit.fill);
+        },
+        itemCount: swiperDataList.length,
+        pagination: new SwiperPagination(),
+        autoplay: true,
+      ),
+    );
   }
 }
