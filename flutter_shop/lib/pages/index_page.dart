4e53016defad1e0f7d77723269b4b6d574795fc4
HaoYi <491493118@qq.com>
2019-03-14 20:01:20 +0800

    保持页面状态

--- flutter_shop/lib/pages/index_page.dart
+++ flutter_shop/lib/pages/index_page.dart
@@ -1,55 +1,72 @@
-import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'home_page.dart';
 import 'category_page.dart';
 import 'cart_page.dart';
 import 'member_page.dart';
-
-
 class IndexPage extends StatefulWidget {
-  final Widget child;
-
-  IndexPage({Key key, this.child}) : super(key: key);
-
   _IndexPageState createState() => _IndexPageState();
 }
-
-class _IndexPageState extends State<IndexPage> {
+class _IndexPageState extends State<IndexPage>{
+   PageController _pageController;
   final List<BottomNavigationBarItem> bottomTabs = [
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),title: Text('首页')),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.search),title: Text('分类')),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.shopping_cart),title: Text('购物车')),
-    BottomNavigationBarItem(icon: Icon(CupertinoIcons.profile_circled),title: Text('会员中心')),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.home),
+      title:Text('首页')
+    ),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.search),
+      title:Text('分类')
+    ),
+    BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.shopping_cart),
+      title:Text('购物车')
+    ),
+     BottomNavigationBarItem(
+      icon:Icon(CupertinoIcons.profile_circled),
+      title:Text('会员中心')
+    ),
   ];
-
-  final List tabBodies = [HomePage(),CategoryPage(),CartPage(),MemberPage()];
-
-  int currentIndex = 0;
-  var currentPage;
+  final List<Widget> tabBodies = [
+    HomePage(),
+    CategoryPage(),
+    CartPage(),
+    MemberPage()
+  ];
+  int currentIndex= 0;
+  var currentPage ;
   @override
   void initState() {
-    currentPage = tabBodies[currentIndex];
+   currentPage=tabBodies[currentIndex];
+   _pageController=new PageController()
+      ..addListener(() {
+        if (currentPage != _pageController.page.round()) {
+          setState(() {
+            currentPage = _pageController.page.round();
+          });
+        }
+  });
     super.initState();
   }
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Color.fromRGBO(244, 245, 245, 1.0),
       bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,//设置底部tab的样式，它有两种样式fixed和shifting，只有超过3个才会有区别，国人的习惯一般是使用fixed
+        type:BottomNavigationBarType.fixed,
         currentIndex: currentIndex,
-        items: bottomTabs,
-        onTap: (index) {
+        items:bottomTabs,
+        onTap: (index){
           setState(() {
-            currentIndex = index;
-            currentPage =tabBodies[currentIndex];
+           currentIndex=index;
+            currentPage =tabBodies[currentIndex]; 
           });
         },
       ),
-      body: currentPage,
+      body: IndexedStack(
+        index: currentIndex,
+        children: tabBodies
+      )
     );
   }
 }
-
