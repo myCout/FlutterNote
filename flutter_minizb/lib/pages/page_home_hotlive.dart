a966c6db6ca616e116b119320ca48451b32d0f99
HaoYi <491493118@qq.com>
2019-04-10 20:00:51 +0800

    继续调试数据

--- flutter_minizb/lib/pages/page_home_hotlive.dart
+++ flutter_minizb/lib/pages/page_home_hotlive.dart
@@ -13,50 +13,63 @@ class _HomeHotLiveState extends State<HomeHotLive> {
   List _bannerArray;
 
   @override
+  Widget build(BuildContext context) {
+    return layout(context);
+  }
+
+  @override
   void initState() {
     super.initState();
     _getHomeBannerData();
   }
 
-  void _getHomeBannerData() async {
+  Future _getHomeBannerData() async {
     // await APPApi.postLoginTmp()
     var bannerData = await APPApi.getHomeBanner();
-    var data =json.decode(bannerData.toString());
-
-    // print('banner + $bannerData');
+    print(bannerData.toString());
+    // print('banner + $string');
     setState(() {
-      _bannerArray = (data['d']['carousel'] as List).cast();
+      // var jsonStr = JSON.decode(bannerData);
+      _bannerArray = bannerData['d']['carousel'] ;//(bannerData['d']['carousel'] as List).cast();
     });
   }
-  @override
-  Widget build(BuildContext context) {
-    // var data =json.decode(snapshot.data.toString());
-    List<Map> swiperDataList = _bannerArray;// = (data['data']['slides'] as List).cast();//轮播
-    return Container(
-      child: ListView.builder(
-      //   children: <Widget>[
-      //   SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
-      //     Image.network(  //网络图片
-      //   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546851657199&di=fdd278c2029f7826790191d59279dbbe&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112cb554438090000019ae93094f1.jpg%401280w_1l_2o_100sh.jpg',
-      //   scale: 2.0,  //进行缩放
-      //   fit:BoxFit.cover  // 充满父容器
-      // ),
-      //     Image.network(  //网络图片
-      //   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546851657199&di=fdd278c2029f7826790191d59279dbbe&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112cb554438090000019ae93094f1.jpg%401280w_1l_2o_100sh.jpg',
-      //   scale: 2.0,  //进行缩放
-      //   fit:BoxFit.cover  // 充满父容器
-      // ),
-      //     Image.network(  //网络图片
-      //   'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546851657199&di=fdd278c2029f7826790191d59279dbbe&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112cb554438090000019ae93094f1.jpg%401280w_1l_2o_100sh.jpg',
-      //   scale: 2.0,  //进行缩放
-      //   fit:BoxFit.cover  // 充满父容器
-      // ),
-      //     // SwiperDiy(swiperDataList:swiperDataList ),   //页面顶部轮播组件
-      //   ], 
-        itemBuilder: (BuildContext context, int index) {
-            SwiperDiy(swiperDataList:swiperDataList );
-        },
-      ),
+
+  Widget layout(BuildContext context) {
+    return new Scaffold(
+      appBar: buildAppBar(context),
+      body:
+          new ListView.builder(itemCount: _bannerArray.length, itemBuilder: itemView),
     );
   }
+
+  Widget buildAppBar(BuildContext context) {
+    return new AppBar(title: const Text('历史今日'));
+  }
+
+  Widget itemView(BuildContext context, int index) {
+      // Model model = this._items[index];
+      //设置分割线
+      if (index.isOdd) return new Divider(height: 2.0);
+      return Container(
+        child: SwiperDiy(swiperDataList:_bannerArray)
+      );
+    }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   // var data =json.decode(snapshot.data.toString());
+  //   List<Map> swiperDataList = _bannerArray;// = (data['data']['slides'] as List).cast();//轮播
+  //   return Container(
+  //     child: ListView(
+  //       children: <Widget>[
+  //         // SwiperDiy(swiperDataList:swiperDataList ),
+  //         Image.network(  //网络图片
+  //           'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1546851657199&di=fdd278c2029f7826790191d59279dbbe&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F0112cb554438090000019ae93094f1.jpg%401280w_1l_2o_100sh.jpg',
+  //           scale: 2.0,  //进行缩放
+  //           fit:BoxFit.cover  // 充满父容器
+  //         ),
+  //       ],
+  //     ),
+  //   );
+  // }
 }
